b1d5cb392515a3c690097b746068ff2d881da5d7
Candice Filar <couch2coders@gmail.com>
2025-07-12 09:18:07 -0400

    updates

--- code/bert_nlp.py
+++
@@ -1,6 +0,0 @@
-from transformers import pipeline
-
-classifier = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
-labels = ["Beverage", "Dessert", "Sandwich", "Breakfast", "Salad"]
-result = classifier("egg and cheese bagel", labels)
-print(result)
